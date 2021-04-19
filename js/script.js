426feba8116eb654f42809688d551c9c9e26208a
Daniele <bambudio@libero.it>
2021-04-19 17:51:41 +0200

    Prima impostazione footer completa

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ var app = new Vue (
         el: '#root',
         data: {
             linkNav: ['HOME', 'COURSES', 'INSTRUCTORS', 'EVENTS', 'PAGES', 'ELEMENTS'],
-            dotIndex: '3'
+            dotIndex: '3',
+            // footerLink: {
+            //     {
+            //         name: 'iAcademy',
+            //         link: [
+            //             'Providing Life Changing Experiences Through Education. Class Tha Fit Your Busy Life. Closer to Home'
+            //         ]
+            //     }
+            // }
+                
+            
         },
         methods:{
             sliderBack: function (dot){
