9b90e8c2c72bef1181178bc2127605d9cc4b1382
Daniele <bambudio@libero.it>
2021-04-19 19:55:04 +0200

    Section categories & home-tutoring complete

--- js/script.js
+++ js/script.js
@@ -4,7 +4,35 @@ var app = new Vue (
         el: '#root',
         data: {
             linkNav: ['HOME', 'COURSES', 'INSTRUCTORS', 'EVENTS', 'PAGES', 'ELEMENTS'],
-            dotIndex: '3',
+            dotIndex: 3,
+            categoriesList:[
+                {
+                    name: 'Languages',
+                    icon: '1.png'
+                },
+                {
+                    name: 'Software',
+                    icon: '2.png'
+                },
+                {
+                    name: 'Business',
+                    icon: '3.png'
+                },
+                {
+                    name: 'Chemistry',
+                    icon: '4.png'
+                },
+                {
+                    name: 'Science',
+                    icon: '5.png'
+                },
+                {
+                    name: 'DIY&Craft',
+                    icon: '6.png'
+                },
+
+            ],
+            autoplayHeader: null
             // footerLink: {
             //     {
             //         name: 'iAcademy',
@@ -15,6 +43,16 @@ var app = new Vue (
             // }
                 
             
+        },
+        mounted: function(){
+            this.autoplayHeader = setInterval( ()=> {
+                if ( this.dotIndex == 3){
+                    this.dotIndex = 1;
+                } else {
+                    this.dotIndex++;
+                }
+                
+            }, 2000);
         },
         methods:{
             sliderBack: function (dot){
