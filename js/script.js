8bb9b1736e960a27de5007f70244cdd93130394f
Daniele <bambudio@libero.it>
2021-04-19 16:06:37 +0200

    Aggiunto slider header

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ var app = new Vue (
         el: '#root',
         data: {
             linkNav: ['HOME', 'COURSES', 'INSTRUCTORS', 'EVENTS', 'PAGES', 'ELEMENTS'],
+            dotIndex: '3'
+        },
+        methods:{
+            sliderBack: function (dot){
+                this.dotIndex = dot;
+            }
         }
 });
